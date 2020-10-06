d9954485088fde0260c1bd95c80f725f2cb64619
UnknownBoom <artem2001sarg@mail.ru>
2020-10-06 12:27:40 +0300

    Init commit

---
+++ src/test/java/com/SpringBootProject/OurApp/OurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootProject.OurApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
