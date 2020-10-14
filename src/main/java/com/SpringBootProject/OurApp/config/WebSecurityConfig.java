3d1f2aee6865beddb54c173d9139b91d61846914
UnknownBoom <artem2001sarg@mail.ru>
2020-10-14 21:04:00 +0300

    bug fix

--- src/main/java/com/SpringBootProject/OurApp/config/WebSecurityConfig.java
+++ src/main/java/com/SpringBootProject/OurApp/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/login","/registration","/images/**", "/bootstrap/**","/fragments/**","/css/**").permitAll()
+                .antMatchers("/","/login","/registration_login/**","/registration","/images/**", "/bootstrap/**","/fragments/**","/css/**").permitAll()
                 .anyRequest().authenticated();
         http
                 .formLogin()
--- src/main/java/com/SpringBootProject/OurApp/repo/OrdersRepo.java
+++ src/main/java/com/SpringBootProject/OurApp/repo/OrdersRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OrdersRepo extends JpaRepository<Orders,Long> {
     Orders findById(long id);
-    @Query(value = "SELECT o.order_id,o.order_date,o.order_name,o.order_schemes,o.planed_date_end,o.price,o.customer_id,o.manager_id,o.product_type_id FROM users u INNER JOIN orders o ON u.id = o.customer_id OR u.id = o.manager_id where u.id = ?1",nativeQuery = true)
+    @Query(value = "SELECT o.order_id,o.order_date,o.order_name,o.order_scheme_name,o.planed_date_end,o.price,o.customer_id,o.manager_id,o.product_type_id FROM users u INNER JOIN orders o ON u.id = o.customer_id OR u.id = o.manager_id where u.id = ?1",nativeQuery = true)
     Iterable<Orders> findOrdersByUser_idList(Long id);
     @Query(value = "select  * from orders where order_name = ?1",nativeQuery = true)
     Orders findByOrder_name(String order_name);
