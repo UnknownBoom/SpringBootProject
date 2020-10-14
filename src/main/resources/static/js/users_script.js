4e6c2e9e2fde17bde673f72b57773d44a197a687
TwoQ4 <64400857+TwoQ4@users.noreply.github.com>
2020-10-14 19:22:52 +0300

    Сделал, чтобы нормально работал select

--- src/main/resources/static/js/users_script.js
+++ src/main/resources/static/js/users_script.js
@@ -1,20 +1,38 @@
-let options = document.querySelectorAll("select");
-console.log(options);
+let selects1 = document.querySelectorAll("select#rolesSelect1");
+let selects3 = document.querySelectorAll("select#rolesSelect3");
 
-let input = document.querySelectorAll("input#rolesData");
-console.log(input);
+console.log(selects1);
+console.log(selects3);
 
-let roles_event_listener = function(e) {
 
-    input.name = $("#rolesSelect option:selected").text().toLowerCase();
-    console.log(input.name);
+let input1 = document.querySelectorAll("input#rolesData1");
+let input3 = document.querySelectorAll("input#rolesData3");
+console.log(input1);
+console.log(input3);
+
+let roles_event_listener1 = function(e) {
+
+    input1.value = $("#rolesSelect1 option:selected").val().toLowerCase();
+    console.log(input1.value);
+
+
+};
+let roles_event_listener3 = function(e) {
+
+    input3.value = $("#rolesSelect3 option:selected").val().toLowerCase();
+    console.log(input3.value);
 
 
 };
 
-for(let i = 0; i < options.length; i++){
-    options[i].addEventListener('change', roles_event_listener)
+for(let i = 0; i < selects1.length; i++){
+    selects1[i].addEventListener('change', roles_event_listener1);
 }
+for(let i = 0; i < selects3.length; i++){
+    selects3[i].addEventListener('change', roles_event_listener3);
+}
+
+
 
 
 
