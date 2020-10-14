e872b50a7cf723298c0cb7ad308d7a1ab23a11ca
56q <34@mail.ru>
2020-10-14 17:52:31 +0300

    add users

--- src/main/resources/static/js/users_script.js
+++ src/main/resources/static/js/users_script.js
@@ -1,1 +1,20 @@
+let options = document.querySelectorAll("select");
+console.log(options);
+
+let input = document.querySelectorAll("input#rolesData");
+console.log(input);
+
+let roles_event_listener = function(e) {
+
+    input.name = $("#rolesSelect option:selected").text().toLowerCase();
+    console.log(input.name);
+
+
+};
+
+for(let i = 0; i < options.length; i++){
+    options[i].addEventListener('change', roles_event_listener)
+}
+
+
 
